f590cdf7fa67b2db2d6107795d19c57d967fa016
h.ell.o <hellersdorfer@cygnecode.com>
2012-07-18 11:44:46 +0200

    test java solution

---
+++ test1/t1/src/Test1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: hellersdorfer
+ * Date: 18.07.12
+ * Time: 11:44
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test1 {
+}
